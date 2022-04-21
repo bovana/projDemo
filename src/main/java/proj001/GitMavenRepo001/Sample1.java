8849d3a15b0a7a406eb601a584dd63d7adce6e43
Arpana <muriki.arpana@gmail.com>
2022-04-21 16:03:35 +0530

    first commit

---
+++ src/main/java/proj001/GitMavenRepo001/Sample1.java
@@ -0,0 +1,10 @@
+package proj001.GitMavenRepo001;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to Git and Github...");
+	}
+
+}
