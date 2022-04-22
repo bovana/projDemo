0cc780b81f8a724a8d44a25136f918872f41e036
Arpana <muriki.arpana@gmail.com>
2022-04-22 16:09:01 +0530

    third commit

---
+++ src/main/java/proj001/GitMavenRepo001/Sample2.java
@@ -0,0 +1,10 @@
+package proj001.GitMavenRepo001;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is my second class");
+	}
+
+}
